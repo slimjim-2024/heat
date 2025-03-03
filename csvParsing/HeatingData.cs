779dcb79b8d40b24cfd00bf9aaa3462ef9e9586e
gojamont <goja.mont@gmail.com>
2025-03-03 13:51:41 +0100

    fixed the CultureInfo causing the program not to work

--- csvParsing/HeatingData.cs
+++ csvParsing/HeatingData.cs
@@ -1,18 +1,21 @@
 // Note for the csv file
 //  [0] is time from,  [1] is time to, [2] is heat demand, [3] is an Electricity price
+using System;
+using System.Globalization;
 
 namespace HeatingOptimizer;
 class HeatingData
 {
+
     protected internal static List<Timeframe> WinterTimeFrame = new List<Timeframe>(); // list of timeframes
     protected internal static List<Timeframe> SummerTimeFrame = new List<Timeframe>(); // list of timeframes
+    private static string? line;
     public static void GetData(string path = "heating_data.csv") // function for getting data from csv file
     {
         try
         {
             using (StreamReader sr = new StreamReader(path)) //reads the .csv file
             {
-                string? line;
                 // skip the first 3 lines
                 sr.ReadLine();
                 sr.ReadLine();
@@ -23,15 +26,17 @@ class HeatingData
                     string[] periods = line.Split(",,"); // period from the csv file
                     string[] winter = periods[0].Split(","); // columns inside of the period in csv for WINTER
                     string[] summer = periods[1].Split(","); // columns inside of the period in csv for SUMMER
-                    WinterTimeFrame.Add(new Timeframe(DateTime.Parse(winter[0]), DateTime.Parse(winter[1]), double.Parse(winter[2]), decimal.Parse(winter[3]))); // adds the data to the list
-                    SummerTimeFrame.Add(new Timeframe(DateTime.Parse(summer[0]), DateTime.Parse(summer[1]), double.Parse(summer[2]), decimal.Parse(summer[3]))); // adds the data to the list
+                    WinterTimeFrame.Add(new Timeframe(DateTime.Parse(winter[0]), DateTime.Parse(winter[1]), Convert.ToDouble(winter[2], CultureInfo.InvariantCulture), Convert.ToDecimal(winter[3], CultureInfo.InvariantCulture))); // adds the data to the list
+                    SummerTimeFrame.Add(new Timeframe(DateTime.Parse(summer[0]), DateTime.Parse(summer[1]), Convert.ToDouble(summer[2], CultureInfo.InvariantCulture), Convert.ToDecimal(summer[3], CultureInfo.InvariantCulture))); // adds the data to the list
                 }
             }
         }
         catch (Exception e) // if exception appears, then return a message that file could not be read. 
         {
+            Console.WriteLine($"The line was {line ?? "null"}");
             Console.WriteLine("The file could not be read:");
             Console.WriteLine(e.Message);
+
         }
     }
     public static List<ProductionUnit> GetProductionUnits(string fileName)
@@ -46,7 +51,7 @@ class HeatingData
                 while ((line = sr.ReadLine()) != null)
                 {
                     string[] data = line.Split(",");
-                    productionUnits.Add(new ProductionUnit(data[0], double.Parse(data[1]), double.Parse(data[2]), decimal.Parse(data[3]), int.Parse(data[4]), double.Parse(data[5])));
+                    productionUnits.Add(new ProductionUnit(data[0], double.Parse(data[1], CultureInfo.InvariantCulture), double.Parse(data[2], CultureInfo.InvariantCulture), decimal.Parse(data[3], CultureInfo.InvariantCulture), int.Parse(data[4]), double.Parse(data[5], CultureInfo.InvariantCulture)));
                 }
             }
         }
