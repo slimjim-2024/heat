0617beb4608564072aa74de0bb5dcc1f64b37884
Ktoto198 <frybenderer43@gmail.com>
2025-03-03 14:11:52 +0100

    Co-authored-by: gojamont <gojamont@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ namespace HeatingOptimizer;
 class Program
 {
     
-    // [STAThread]
-    // public static void Main(string[] args) => BuildAvaloniaApp()
-    //     .StartWithClassicDesktopLifetime(args);
-    // // Avalonia configuration, don't remove; also used by visual designer.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+    // Avalonia configuration, don't remove; also used by visual designer.
 
-    // public static AppBuilder BuildAvaloniaApp()
-    //     => AppBuilder.Configure<App>()
-    //         .UsePlatformDetect()
-    //         .WithInterFont()
-    //         .LogToTrace();
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 
-    public static void Main(string[] args) => Init.Initialize();
+    //public static void Main(string[] args) => Init.Initialize();
 }
 
