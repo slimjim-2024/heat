29609dc33c6a97fc1674403a20b160918949e5c3
anpaz24 <anpaz24@student.sdu.dk>
2025-03-11 14:48:11 +0100

    Changes to CostCalculator and ProdUnitSorter

--- CostCalculator.cs
+++ CostCalculator.cs
@@ -1,45 +1,51 @@
 
+using Avalonia.Reactive;
+
 namespace HeatingOptimizer
 {
     class CostCalculator
     {
-        public static void CalculateTimeframe(Timeframe timeframe, List<ProductionUnit> prodUnits)
+        private static void CalculateTimeframe(Timeframe timeframe, List<ProductionUnit> prodUnits)
         {
             double remainingHeat = timeframe.HeatDemand;
-                foreach (var prodUnit in prodUnits)
-                {
-                    // Calculate heat
-                    double heatProduced = Math.Min(remainingHeat, prodUnit.MaxHeatOutput);
-                    prodUnit.SeasonHeatProduction.Append(heatProduced);
-                    remainingHeat -= heatProduced;
+            foreach (var prodUnit in prodUnits)
+            {
+                // Calculate heat
+                double heatProduced = Math.Min(remainingHeat, prodUnit.MaxHeatOutput);
+                remainingHeat -= heatProduced;
+                
+                // Calculate electricity // Check w/ supervisor if formulae is right
+                double fraction = heatProduced / prodUnit.MaxHeatOutput;
+                double electricityProduced = fraction * prodUnit.MaxElectricity;
 
-                    // Calculate electricity
-                    // Check w/ teacher if formulae is right
-                    double fraction = heatProduced / prodUnit.MaxHeatOutput;
-                    double electricityProduced = fraction * prodUnit.MaxElectricity;
+                // Calculate cost
+                decimal cost = (decimal)(heatProduced*(double)prodUnit.ProductionCosts - electricityProduced*(double)timeframe.ElectricityPrice);
 
-                    // Calculate cost
-                    prodUnit.SeasonProductionCosts.Append(
-                        heatProduced*(double)prodUnit.ProductionCosts - electricityProduced*(double)timeframe.ElectricityPrice
-                        );
-                }
+                // Also record Gas Consumption?
+
+                // Process results
+                prodUnit.SeasonHeatProduction.Append(heatProduced);
+                prodUnit.SeasonElectricityProduction.Append(electricityProduced);
+                prodUnit.SeasonProductionCosts.Append(cost);
+            }
         }
 
-        // prodUnits is an already sorted list of the machines in use
-        public static void CalculateSeason(List<ProductionUnit> prodUnits, List<Timeframe> Season)
+        public static void CalculateSeason(List<ProductionUnit> prodUnits, List<Timeframe> season, short sortType)
         {
             // Makes prodUnit.SeasonHeatProduction empty before calculation
             foreach (var prodUnit in prodUnits) prodUnit.SeasonHeatProduction = [];
-
-            foreach (Timeframe timeframe in Season)
+            
+            for (int i = 0; i < season.Count; i++) // Loop through each timeframe
             {
-                /* 
-                When trying to get the cheapest solution,
-                timeframes' electricity price should be taken into account:
-                if (any prodUnit produces electricity && calculating cheapest solution)
-                    Sort prodUnits;
+                /*
+                Sort prodUnits on first timeframe
+                ||
+                Sort on every timeframe if looking for cheapest solution,
+                as electicity prices change every timeframe
                 */
-                CalculateTimeframe(timeframe, prodUnits);
+                if (i==1 || sortType==0 /*cheapest*/)
+                    prodUnits = ProdUnitSorter.Sort(prodUnits, season[i], sortType);
+                CalculateTimeframe(season[i], prodUnits);
             }
         }
     }
--- ProdUnitSorter.cs
+++ ProdUnitSorter.cs
@@ -1,26 +1,25 @@
 namespace HeatingOptimizer;
 
-static class ProductionUnitSorter
+static class ProdUnitSorter
 {
-    public static List<ProductionUnit> Sort(List<ProductionUnit> pUnits, Timeframe tf, int sortType)
+    /*
+    public enum SortType
     {
-        switch (sortType)
+        Cheapest,
+        MoreGasEfficient(LessConsumption),
+        LessCO2Emissions
+    }
+    */
+
+    public static List<ProductionUnit> Sort(List<ProductionUnit> pUnits, Timeframe timeframe, short sortType)
+    {
+        return sortType switch
         {
-            case 0:
-            return pUnits.OrderBy(x => x.CO2Emissions).ToList(); // Less emissions
-
-            case 1:
-            return pUnits.OrderBy(x => x.Consumption).ToList(); // Less consumption
-
-            case 2:
-            // (Heat cost - Electricity return)/Total heat production = cost per unit of heat
-            return pUnits.OrderBy(x => (x.MaxHeatOutput*(double)x.ProductionCosts - x.MaxElectricity*(double)tf.ElectricityPrice)/x.MaxHeatOutput).ToList(); // Cheapest
-
-            // Not sure if this is something
-            // return pUnits.OrderByDescending(x => (double)x.ProductionCosts/x.Consumption).ToList(); // More efficient
-
-            default:
-            return []; // Shouldn't happen
-        } 
+            // (Heat cost - Electricity return)/Total heat production = cost/return per unit of heat
+            0 => pUnits.OrderBy(x => (x.MaxHeatOutput * (double)x.ProductionCosts - x.MaxElectricity * (double)timeframe.ElectricityPrice) / x.MaxHeatOutput).ToList(),
+            1 => pUnits.OrderBy(x => x.CO2Emissions).ToList(),// Less emissions
+            2 => pUnits.OrderBy(x => x.Consumption).ToList(),// Less consumption
+            _ => [] // Throw exception?
+        };
     }
 }
