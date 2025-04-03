bc52c66f86c54c1181c9eafaab4a55bda2365fd6
Dalius69 <dalius11d@gmail.com>
2025-04-03 13:30:46 +0200

    prettify

--- HeatingOptimizer/UserInterface/MainWindow/MainWindowViewModel.cs
+++ HeatingOptimizer/UserInterface/MainWindow/MainWindowViewModel.cs
@@ -20,7 +20,7 @@ namespace HeatingOptimizer.ViewModels
     public partial class MainWindowViewModel : ObservableObject
     {
         private Random rand = new Random();
-        protected internal Dictionary<string, SKColor> colorDict=new()
+        protected internal Dictionary<string, SKColor> colorDict = new()
         {
             {"GB1", SKColors.LimeGreen},
             {"GB2", SKColors.Yellow},
@@ -30,13 +30,14 @@ namespace HeatingOptimizer.ViewModels
         };
 
         [ObservableProperty]
-        private List<string> _seasonSelection = ["Summer","Winter"];
+        private List<string> _seasonSelection = ["Summer", "Winter"];
         protected internal Dictionary<string, Results> ResultDictionary = new Dictionary<string, Results>();
         [ObservableProperty]
-        private string _inputText= string.Empty;
+        private string _inputText = string.Empty;
 
         [ObservableProperty]
         private string _selectedSeason = "Winter";
+
 
         [ObservableProperty]
         private ObservableCollection<ISeries> _series = [];
@@ -97,17 +98,18 @@ namespace HeatingOptimizer.ViewModels
         }
         ];
         [RelayCommand]
-    public void PressedCommand(PointerCommandArgs args)
-    {
-        var foundPoints = args.Chart.GetPointsAt(args.PointerPosition);
-        PieSeries.Clear();
-        // foreach (var point in foundPoints)
-        // {
-        var point = foundPoints.FirstOrDefault();
+        public void PressedCommand(PointerCommandArgs args)
+        {
+            var foundPoints = args.Chart.GetPointsAt(args.PointerPosition);
+            PieSeries.Clear();
+            // foreach (var point in foundPoints)
+            // {
+            var point = foundPoints.FirstOrDefault();
             if (point == null) return;
 
-            var resultValues = ResultDictionary.Where(s=>s.Value.HeatProduced[point.Index] > 0).ToList();
-            foreach(var item in resultValues){
+            var resultValues = ResultDictionary.Where(s => s.Value.HeatProduced[point.Index] > 0).ToList();
+            foreach (var item in resultValues)
+            {
                 PieSeries.Add(new PieSeries<double>()
                 {
                     Name = item.Key,
@@ -117,41 +119,41 @@ namespace HeatingOptimizer.ViewModels
                     DataLabelsSize = 20,
                 });
             }
-        // }
-    }
-
-    [RelayCommand]
-    public void GenerateButton_Click(string sender)
-    {
-        if (sender.GetType() == typeof(string))
-        {
-            Console.WriteLine("Sender is a string, not a button.");
-        }
-        // Returns if no machine or heating data
-        if (SelectedProductionUnits.Count == 0 || Frames is null) return;
-
-        Series.Clear();
-        CostCalculator.CalculateSeason(SelectedProductionUnits, Frames[sender],
-        SelectedIndex, ref ResultDictionary);
-
-        // Displays timeframes on x axis
-        XAxes[0].Labels = [.. Frames[sender].Select(TF => TF.TimeFrom.ToString("dd/MM H:mm"))];
-        XAxes[0].LabelsRotation = 90;
-        XAxes[0].LabelsDensity=0;
-        XAxes[0].TextSize=10;
-        XAxes[0].MinStep=1;
-
-        Series.Add(
-           new LineSeries<double>{Name="Winter Heat Demand" ,Values=new ObservableCollection<double>(Frames[sender].Select(s=> s.HeatDemand)), Fill=null, }
-        );
-        foreach (var PU in ResultDictionary)
-        {
-            Series.Add(new StackedAreaSeries<double>{Name=PU.Key, Values=PU.Value.HeatProduced, Fill= new SolidColorPaint{Color = colorDict[PU.Key]}, });
+            // }
         }
 
-        XAxes[0].MinLimit = 0;
-        YAxes[0].MinLimit = 0;
-    }
+        [RelayCommand]
+        public void GenerateButton_Click(string sender)
+        {
+            if (sender.GetType() == typeof(string))
+            {
+                Console.WriteLine("Sender is a string, not a button.");
+            }
+            // Returns if no machine or heating data
+            if (SelectedProductionUnits.Count == 0 || Frames is null) return;
+
+            Series.Clear();
+            CostCalculator.CalculateSeason(SelectedProductionUnits, Frames[sender],
+            SelectedIndex, ref ResultDictionary);
+
+            // Displays timeframes on x axis
+            XAxes[0].Labels = [.. Frames[sender].Select(TF => TF.TimeFrom.ToString("dd/MM H:mm"))];
+            XAxes[0].LabelsRotation = 90;
+            XAxes[0].LabelsDensity = 0;
+            XAxes[0].TextSize = 10;
+            XAxes[0].MinStep = 1;
+
+            Series.Add(
+               new LineSeries<double> { Name = "Winter Heat Demand", Values = new ObservableCollection<double>(Frames[sender].Select(s => s.HeatDemand)), Fill = null, }
+            );
+            foreach (var PU in ResultDictionary)
+            {
+                Series.Add(new StackedAreaSeries<double> { Name = PU.Key, Values = PU.Value.HeatProduced, Fill = new SolidColorPaint { Color = colorDict[PU.Key] }, });
+            }
+
+            XAxes[0].MinLimit = 0;
+            YAxes[0].MinLimit = 0;
+        }
         protected internal Dictionary<string, List<TimeFrame>> Frames;
 
         public List<ProductionUnit> AllProductionUnits { get; set; } = DataParser.ParseMachineData();
@@ -160,7 +162,7 @@ namespace HeatingOptimizer.ViewModels
         // checking which of the options is selected by their index
 
         [ObservableProperty]
-       private short _selectedIndex = 0; // Default to first index
+        private short _selectedIndex = 0; // Default to first index
 
         public MainWindowViewModel()
         {
@@ -205,6 +207,6 @@ namespace HeatingOptimizer.ViewModels
         }
         */
 
-        
+
     }
 }
